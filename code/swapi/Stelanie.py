0831af7202e2aa995b13fa0dae7e571f7241be8c
Stelanie <80799764+Stelanie@users.noreply.github.com>
2021-05-04 22:18:37 -0400

    Added Leia's last name

--- code/swapi/Stelanie.py
+++ code/swapi/Stelanie.py
@@ -5,5 +5,5 @@ endpoint = 'https://swapi.py4e.com/api'
 response = requests.get(f"{endpoint}/people/", {'search': 'leia'}).json()
 leia_organa = response['results'][0]
 
-print(f"\nLeia = {leia_organa}")
+print(f"\nLeia_Organa = {leia_organa}")
 
