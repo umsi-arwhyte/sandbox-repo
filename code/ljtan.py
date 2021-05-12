be1e75c9dc7a8ab9618525d0344e387d6ce3b0e7
Anthony Whyte <anthwhyte@gmail.com>
2021-05-11 21:58:27 -0400

    Add Chewbacca

--- code/ljtan.py
+++ code/ljtan.py
@@ -6,3 +6,8 @@ response = requests.get(f"{endpoint}/people/", {'search': 'luke'}).json()
 luke_skywalker = response['results'][0]
 
 print(f"\nLuke Skywalker = {luke_skywalker}")
+
+response = requests.get(f"{endpoint}/people/", {'search': 'chewbacca'}).json()
+chewbacca = response['results'][0]
+
+print(f"\nChewbacca = {chewbacca}")
